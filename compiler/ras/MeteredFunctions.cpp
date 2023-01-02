293af8952d7157be92f61a9be82b3913277f1aa3
Daryl Maier <maier@ca.ibm.com>
2023-01-02 14:00:39 -0700

    Fix
    Signed-off-by: Daryl Maier <maier@ca.ibm.com>

--- compiler/ras/MeteredFunctions.cpp
+++ compiler/ras/MeteredFunctions.cpp
@@ -1,4 +1,5 @@
 #include "ras/MeteredFunctions.hpp"
+#include <time.h>
 
 static const char *meteredFunctionName[] =
 {
@@ -7,3 +8,15 @@ static const char *meteredFunctionName[] =
 #undef METERED_FUNCTION
 };
 
+TR::FunctionMeter:FunctionMeter(MeteredFunction f, TR::Compilation *comp) : _mf(f), _comp(comp) {
+   comp->totalCalls[f]++;
+   clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &_startTime);
+}
+
+~TR::FunctionMeter::FunctionMeter() {
+   struct timespec end_time;
+   clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end_time);
+   int64_t diffInNanos = (end_time.tv_sec - _start_time.tv_sec) * (int64_t)1e9 + (end_time.tv_nsec - _start_time.tv_nsec);
+
+   _comp->elapsedTimeInNanos[_mf] = diffInNanos;
+}
--- compiler/ras/MeteredFunctions.hpp
+++ compiler/ras/MeteredFunctions.hpp
@@ -20,20 +20,10 @@ private:
    TR::Compilation *_comp;
 
 public:
-   FunctionMeter(MeteredFunction f, TR::Compilation *comp) : _mf(f), _comp(comp) {
-      comp->totalCalls[f]++;
-      clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &_startTime);
-   }
+   FunctionMeter(MeteredFunction f, TR::Compilation *comp);
+   ~FunctionMeter();
 
-   ~FunctionMeter() {
-      struct timespec end_time;
-      clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end_time);
-      int64_t diffInNanos = (end_time.tv_sec - _start_time.tv_sec) * (int64_t)1e9 + (end_time.tv_nsec - _start_time.tv_nsec);
-
-      _comp->elapsedTimeInNanos[_mf] = diffInNanos;
-   }
-
-}
+};
 
 }
 
