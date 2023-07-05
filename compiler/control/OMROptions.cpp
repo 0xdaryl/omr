5f310f9e10743a8ab330271d03916f418a04ec5e
Daryl Maier <maier@ca.ibm.com>
2023-07-05 19:44:16 -0400

    Add enableCodeMetaData option
    Signed-off-by: Daryl Maier <maier@ca.ibm.com>

--- compiler/control/OMROptions.cpp
+++ compiler/control/OMROptions.cpp
@@ -671,6 +671,7 @@ TR::OptionTable OMR::Options::_jitOptions[] = {
    {"enableClassChainSharing",            "M\tenable class sharing", SET_OPTION_BIT(TR_EnableClassChainSharing), "F", NOT_IN_SUBSET},
    {"enableClassChainValidationCaching",  "M\tenable class chain validation caching", SET_OPTION_BIT(TR_EnableClassChainValidationCaching), "F", NOT_IN_SUBSET},
    {"enableCodeCacheConsolidation",       "M\tenable code cache consolidation", SET_OPTION_BIT(TR_EnableCodeCacheConsolidation), "F", NOT_IN_SUBSET},
+   {"enableCodeMetaDataManager",          "M\tenable code meta data manager", SET_OPTION_BIT(TR_EnableCodeMetaDataManager), "F", NOT_IN_SUBSET},
    {"enableColdCheapTacticalGRA",         "O\tenable cold cheap tactical GRA", SET_OPTION_BIT(TR_EnableColdCheapTacticalGRA), "F"},
    {"enableCompilationBeforeCheckpoint",  "C\tenable compilation before checkpoint", RESET_OPTION_BIT(TR_DisableCompilationBeforeCheckpoint), "F", NOT_IN_SUBSET},
    {"enableCompilationSpreading",         "C\tenable adding spreading invocations to methods before compiling", SET_OPTION_BIT(TR_EnableCompilationSpreading), "F", NOT_IN_SUBSET},
