501926121744218cd3760fadf16ba675afa5ac4a
Daryl Maier <maier@ca.ibm.com>
2026-04-12 08:31:54 -0400

    Replace x86 generateRegisterDependencyConditions() with REGDEPS()
    * improve code readability by introducing a more concise form
    * encapsulate functions into TR:: namespace
    
    The original generateRegisterDependencyCondition() functions are not removed
    yet.  They will be removed when all known downstream project work to use the
    new functions is completed.
    
    Signed-off-by: Daryl Maier <maier@ca.ibm.com>

--- compiler/x/codegen/OMRRegisterDependency.cpp
+++ compiler/x/codegen/OMRRegisterDependency.cpp
@@ -1026,3 +1026,18 @@ TR::RegisterDependencyConditions *generateRegisterDependencyConditions(TR::Node 
 {
     return new (cg->trHeapMemory()) TR::RegisterDependencyConditions(node, cg, additionalRegDeps);
 }
+
+namespace TR {
+
+TR::RegisterDependencyConditions *REGDEPS(uint32_t numPreConds, uint32_t numPostConds,
+    TR::CodeGenerator *cg)
+{
+    return new (cg->trHeapMemory()) TR::RegisterDependencyConditions(numPreConds, numPostConds, cg->trMemory());
+}
+
+TR::RegisterDependencyConditions *REGDEPS(TR::Node *node, TR::CodeGenerator *cg, uint32_t additionalRegDeps)
+{
+    return new (cg->trHeapMemory()) TR::RegisterDependencyConditions(node, cg, additionalRegDeps);
+}
+
+}
